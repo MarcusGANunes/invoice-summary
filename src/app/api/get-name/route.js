2db10f5753ca1d13ed5173a9b0267b10d122b7ca
Marcus Nunes <marcusnunes_11@hotmail.com>
2024-08-21 13:47:42 -0300

    added origin o headers

--- src/app/api/get-name/route.js
+++ src/app/api/get-name/route.js
@@ -14,7 +14,7 @@ export async function GET(req) {
     response.headers.set('Access-Control-Allow-Origin', '*');
     response.headers.set('Access-Control-Allow-Credentials', 'true');
     response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    response.headers.set('Access-Control-Allow-Headers', 'Origin, Content-Type, Authorization, Accept');
 
     return response;
 
