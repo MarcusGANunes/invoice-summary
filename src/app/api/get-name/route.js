fc54def117c987812e53159428f45d14a308209a
Marcus Nunes <marcusnunes_11@hotmail.com>
2024-08-21 17:17:45 -0300

    header in next config solution

--- src/app/api/get-name/route.js
+++ src/app/api/get-name/route.js
@@ -1,4 +1,4 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
 
 export async function GET(req) {
@@ -6,31 +6,12 @@ export async function GET(req) {
     const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
 
     if (!token) {
-      return new NextResponse(JSON.stringify({ message: 'Not authenticated' }), { status: 401 });
+      return NextResponse.json({ message: 'Not authenticated' }, { status: 401 });
     }
-
-    const response = new NextResponse(JSON.stringify({ name: token.name }));
-
-    response.headers.set('Access-Control-Allow-Origin', '*');
-    response.headers.set('Access-Control-Allow-Credentials', 'true');
-    response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    response.headers.set('Access-Control-Allow-Headers', 'Origin, Content-Type, Authorization, Accept');
-
-    return response;
-
+    return NextResponse.json({ name: token.name });
+    
   } catch (error) {
     console.error('Error fetching token:', error);
-    return new NextResponse(JSON.stringify({ message: 'An error occurred' }), { status: 500 });
+    return NextResponse.json({ message: 'An error occurred' }, { status: 500 });
   }
-}
-
-export async function OPTIONS(req) {
-  const response = new NextResponse(null, { status: 204 });
-
-  response.headers.set('Access-Control-Allow-Origin', '*');
-  response.headers.set('Access-Control-Allow-Credentials', 'true');
-  response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-
-  return response;
 }
