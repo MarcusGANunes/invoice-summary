9de08620292b7da3d4ba0e17e836152a7f9307ee
Marcus Nunes <marcusnunes_11@hotmail.com>
2024-08-21 17:44:47 -0300

    new source

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   async headers() {
       return [
           {
-              source: "/api/get-name",
+              source: "/api/(.*)",
               headers: [
                   { key: "Access-Control-Allow-Credentials", value: "true" },
                   { key: "Access-Control-Allow-Origin", value: "*" },
