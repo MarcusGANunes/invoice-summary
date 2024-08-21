31581c545079955a8ca3e094ef6d22596773814e
Marcus Nunes <marcusnunes_11@hotmail.com>
2024-08-21 13:18:35 -0300

    cors fixed

--- src/middleware.js
+++ src/middleware.js
@@ -4,6 +4,19 @@ import { getToken } from 'next-auth/jwt';
 export async function middleware(req) {
   const { pathname } = req.nextUrl;
 
+  if (pathname.startsWith('/api/')) {
+    const response = NextResponse.next();
+
+    response.headers.set('Access-Control-Allow-Origin', '*')
+    response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+    response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+
+    if (req.method === 'OPTIONS') {
+      return response
+    }
+    return response
+  }
+
   const protectedRoutes = ['/', '/invoices'];
 
   if (protectedRoutes.includes(pathname)) {
